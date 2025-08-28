176f9478106a1235f5ae01f3ae45fb54ee1178db
Jhockinn <Tobiln@hotmail.dk>
2025-08-28 10:55:52 +0200

    created variables file

---
+++ dat_misc/variables_1.js
@@ -0,0 +1,27 @@
+const name ="Katofel";
+
+//string interpolation
+
+console.log(`Hello ${name} World`, 2+2)
+
+//string literals
+
+const firstGreeting = 'It is a nice day today';
+const secondGreeting = "It's a nice day today";
+const thirdGreeting = `It's a nice day today`;
+
+
+
+//backtics(``) for multiline comments   
+
+//node <file>
+
+//use const if possible
+//otherwise use let
+//data types
+
+//String, booleans, number, bigint, symbol, object, null, undefined.
+
+const person = {
+    name: "Jens" //key-value pair (dette er et js object)
+}
